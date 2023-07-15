47c8be27bda2e2b858cc84e0b6dd26bf4eeb1937
ghghwldk <ghghwldk@gmail.com>
2023-07-15 21:06:11 +0900

    companion object inside controller has been adopt

--- src/main/kotlin/com/andrejusti/example/kotlin/springboot/rest/test/controller/TestController.kt
+++ src/main/kotlin/com/andrejusti/example/kotlin/springboot/rest/test/controller/TestController.kt
@@ -9,10 +9,11 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.annotation.PostConstruct
 
 @RestController
-@RequestMapping("/test")
 @RequiredArgsConstructor
 class TestController(
         val testService: TestService,
@@ -20,13 +21,21 @@ class TestController(
 ){
     val logger: Logger = LoggerFactory.getLogger(TestController::class.java)
 
-    @GetMapping("/1")
+    companion object {
+        const val DOMAIN_URI: String = "/test"
+        const val URI_1: String = "1"
+        const val URI_2: String = "2"
+        const val URL1: String = "$DOMAIN_URI/$URI_1"
+        const val URL2: String = "$DOMAIN_URI/$URI_2"
+    }
+
+    @GetMapping(URL1)
     fun test(): String{
         logger.info("test called")
         return testService.test()
     }
 
-    @GetMapping("/2")
+    @GetMapping(URL2)
     fun test2(): String{
         logger.info("test2 called")
         return testService2.test()
