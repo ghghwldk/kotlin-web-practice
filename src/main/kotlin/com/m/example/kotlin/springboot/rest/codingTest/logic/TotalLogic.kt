a3483afeac3e431ac15623d71d72cf6ec40a49fc
ghghwldk <ghghwldk@gmail.com>
2023-07-16 12:31:36 +0900

    change the code using the kotlin grammar

--- src/main/kotlin/com/m/example/kotlin/springboot/rest/codingTest/logic/TotalLogic.kt
+++ src/main/kotlin/com/m/example/kotlin/springboot/rest/codingTest/logic/TotalLogic.kt
@@ -16,13 +16,23 @@ class TotalLogic {
         this.problemOutput = result
     }
 
-    fun execute(): Int {
-        if (problemOutput is 요격시스템Result) {
-            val count = (problemOutput as 요격시스템Result).count
-            return count
-        } else {
-            println("Invalid result type.")
-            return -1
+    class TotalLogic {
+        private lateinit var problemInput: ProblemInput
+        private lateinit var solution: CodingTestSolution
+        private lateinit var problemOutput: ProblemOutput
+
+        fun set(problemInput: ProblemInput, solution: CodingTestSolution, result: ProblemOutput) {
+            this.problemInput = problemInput
+            this.solution = solution
+            this.problemOutput = result
+        }
+
+        fun execute(): Int {
+            val result = problemOutput as? 요격시스템Result
+                    ?: throw IllegalStateException("Invalid result type.")
+
+            return result.count
         }
     }
+
 }
