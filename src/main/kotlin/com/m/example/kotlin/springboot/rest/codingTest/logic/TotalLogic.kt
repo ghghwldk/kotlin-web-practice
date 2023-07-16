7fbfa0b9990cba2655e4f00df974be6fb0fca308
ghghwldk <ghghwldk@gmail.com>
2023-07-16 12:32:49 +0900

    remove copy & paste mistake

--- src/main/kotlin/com/m/example/kotlin/springboot/rest/codingTest/logic/TotalLogic.kt
+++ src/main/kotlin/com/m/example/kotlin/springboot/rest/codingTest/logic/TotalLogic.kt
@@ -16,23 +16,10 @@ class TotalLogic {
         this.problemOutput = result
     }
 
-    class TotalLogic {
-        private lateinit var problemInput: ProblemInput
-        private lateinit var solution: CodingTestSolution
-        private lateinit var problemOutput: ProblemOutput
+    fun execute(): Int {
+        val result = problemOutput as? 요격시스템Result
+                ?: throw IllegalStateException("Invalid result type.")
 
-        fun set(problemInput: ProblemInput, solution: CodingTestSolution, result: ProblemOutput) {
-            this.problemInput = problemInput
-            this.solution = solution
-            this.problemOutput = result
-        }
-
-        fun execute(): Int {
-            val result = problemOutput as? 요격시스템Result
-                    ?: throw IllegalStateException("Invalid result type.")
-
-            return result.count
-        }
+        return result.count
     }
-
 }
